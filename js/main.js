92d0e1b356b8b581054da02644ec662fed5b06f5
arainabdulwahab <arainabdulwahab6@gmail.com>
2022-09-30 17:44:52 +0200

    javascript toogle added and application is done

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-//alert("welcome");
+function toogleMenu(){
+    let menu = document.getElementById('hiddenItem');
+    if(menu.style.display == 'none'){
+        menu.style.display = 'block';
+    }else{
+        menu.style.display = 'none';
+    }
+}
